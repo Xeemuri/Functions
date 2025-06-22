9fd2b210f8c577442c6d372b56bdcefe3d52a8c6
Илья Щербаков <iliya309@mail.ru>
2025-06-22 23:04:13 +1000

    Encoding fix

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+п»ї#include <iostream>
 using namespace std;
 
 
-int Add(int a, int b, int c = 0);	//Прототип функции (Объявление функции - Function Declaration)
+int Add(int a, int b, int c = 0);	//РџСЂРѕС‚РѕС‚РёРї С„СѓРЅРєС†РёРё (РћР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёРё - Function Declaration)
 int Sub(int a, int b);
 int Mul(int a, int b);
 double Div(int a, int b);
@@ -44,26 +44,26 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	int a, b;
-	//cout << "Введите два числа: "; cin >> a >> b;
-	//cout << a << " + " << b << " = " << Add(a, b, 10) << endl; // использование функции (вызов функции - Function call)
+	//cout << "Р’РІРµРґРёС‚Рµ РґРІР° С‡РёСЃР»Р°: "; cin >> a >> b;
+	//cout << a << " + " << b << " = " << Add(a, b, 10) << endl; // РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ С„СѓРЅРєС†РёРё (РІС‹Р·РѕРІ С„СѓРЅРєС†РёРё - Function call)
 	//cout << a << " - " << b << " = " << Sub(a, b) << endl;
 	//cout << a << " * " << b << " = " << Mul(a, b) << endl;
 	//cout << a << " / " << b << " = " << Div(a, b) << endl;
-	cout << "Введите число: "; cin >> a;
-	cout << "Факториал числа " << a << " = " << Factorial(a) << endl;
-	cout << "Введите число и степень: "; cin >> a >> b;
-	cout << a << " в степени " << b << " = " << Power(a, b) << endl;
+	cout << "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: "; cin >> a;
+	cout << "Р¤Р°РєС‚РѕСЂРёР°Р» С‡РёСЃР»Р° " << a << " = " << Factorial(a) << endl;
+	cout << "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ Рё СЃС‚РµРїРµРЅСЊ: "; cin >> a >> b;
+	cout << a << " РІ СЃС‚РµРїРµРЅРё " << b << " = " << Power(a, b) << endl;
 }
-int Add(int a, int b, int c)	//Реализация функции (Определение функции - Function definition)
+int Add(int a, int b, int c)	//Р РµР°Р»РёР·Р°С†РёСЏ С„СѓРЅРєС†РёРё (РћРїСЂРµРґРµР»РµРЅРёРµ С„СѓРЅРєС†РёРё - Function definition)
 {
-	//Addition - сложение
+	//Addition - СЃР»РѕР¶РµРЅРёРµ
 	c+= a + b;
 	return (double)c;
 }
 
 int Sub(int a, int b)
 {
-	//Subtraction - вычитание
+	//Subtraction - РІС‹С‡РёС‚Р°РЅРёРµ
 	return (double)a - b;
 }
 int Mul(int a, int b)
